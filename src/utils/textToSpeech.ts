63f7a2e1a248531b830470dfe59e576f859f4685
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-05-17 11:07:14 -0300

    fix: update speech rate for text-to-speech functionality

--- src/utils/textToSpeech.ts
+++ src/utils/textToSpeech.ts
@@ -2,7 +2,7 @@ function handleClickAudio(text: string) {
   const speech = new SpeechSynthesisUtterance();
   speech.lang = "pt-BR";
   speech.text = text;
-  speech.rate = 0.8; 
+  speech.rate = 1; 
   window.speechSynthesis.speak(speech);
 };
 
