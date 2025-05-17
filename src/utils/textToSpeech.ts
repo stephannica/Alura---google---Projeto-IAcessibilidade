3aa6135079839fce2748368e7cce324519e5ba1b
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-05-17 17:40:49 -0300

    feat: add speech restart and regex to handle special characters

--- src/utils/textToSpeech.ts
+++ src/utils/textToSpeech.ts
@@ -1,9 +1,34 @@
-function handleClickAudio(text: string) {
-  const speech = new SpeechSynthesisUtterance();
-  speech.lang = "pt-BR";
-  speech.text = text;
-  speech.rate = 1; 
-  window.speechSynthesis.speak(speech);
-};
+let lastSpokenText: string | null = null;
+
+function speakText(text: string): void {
+  if (window.speechSynthesis.speaking) {
+    window.speechSynthesis.cancel();
+  }
+
+  let cleanedText = text;
+
+  cleanedText = cleanedText.replace(/[*_#]/g, '')
+  cleanedText = cleanedText.replace(/\s{2,}/g, ' ')
+  cleanedText = cleanedText.replace(/\s*\*\*\s*/g, '');
+
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  utterance.lang = "pt-BR";
+  utterance.rate = 1;
+
+  lastSpokenText = cleanedText;
+
+  window.speechSynthesis.speak(utterance);
+}
+
+export function restartLastSpokenText(): void {
+    if (lastSpokenText !== null) {
+        speakText(lastSpokenText);
+    }
+}
+
+function handleClickAudio(text: string): void {
+  speakText(text);
+}
 
 export default handleClickAudio;
